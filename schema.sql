c18e50c72b93f3d17dcbbfd426dd635a502a6e3a
xcvbnmas <132822281+xcvbnmas@users.noreply.github.com>
2023-08-20 23:31:52 +0300

    Update schema

--- schema.sql
+++ schema.sql
@@ -5,10 +5,25 @@ CREATE TABLE users (
     role INTEGER
 );
 
-CREATE TABLE courses (
+CREATE TABLE exercises (
     id SERIAL PRIMARY KEY,
     creator_id INTEGER REFERENCES users,
     name TEXT,
+    instructions TEXT,
     visible INTEGER
+    model_answer TEXT
 );
 
+CREATE TABLE answers (
+    id SERIAL PRIMARY KEY,
+    exercise_id INTEGER REFERENCES exercises,
+    user_id INTEGER REFERENCES users,
+    answer TEXT
+);
+
+CREATE TABLE messages (
+    id SERIAL PRIMARY KEY,
+    content TEXT,
+    user_id INTEGER REFERENCES users,
+    sent_at TIMESTAMP
+);
